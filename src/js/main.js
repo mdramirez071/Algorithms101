e1077cfa946386276a0e934f63d184cd3950eac4
mdramirez071 <mdramirez071@gmail.com>
2019-11-26 21:45:51 -0800

    ...

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
-aFunc()
-function aFunc(){
-    console.log('hello bitches');
+const arr = [2,5,8,12,16,23,38,56,72,91];
+
+function binarySearch() {
+    console.log('hello');
 }
+
+// console.log('wchaosdksodk');
+// console.log('check check');
+
+
+
+$("#binary-search").click(() => binarySearch());
